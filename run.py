ac00d7d19f3548d450562003996a2b041a122de7
Wystan Concepcion <concepcionwystan@gmail.com>
2018-07-09 16:49:55 +0800

    integrated everything and added filter by date

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from flaskext.mysql import MySQL
 from flask import Flask, request, render_template
-
+import datetime as dt
 
 
 
@@ -32,6 +32,12 @@ def search():
 	license = request.args.get("license")
 	os = request.args.getlist("os")
 	multiple = request.args.get("multiple")
+	start = request.args.get("start")
+	end = request.args.get("end")
+
+
+	
+
 
 
 	osQuery = ""
@@ -41,7 +47,7 @@ def search():
 	osQuery = "" if osQuery == "" else "(" + osQuery + ") and"
 	versionQuery = "" if version == "" else " app_version = '" + version + "' and"
 	platform_versionQuery = "" if platform_version == "" else " platform_version = '" + platform_version + "' and"
-	devicesQuery = "SELECT user_id,email,name,platform,platform_version,app_version from devices WHERE " + osQuery + versionQuery + platform_versionQuery
+	devicesQuery = "SELECT user_id from devices WHERE " + osQuery + versionQuery + platform_versionQuery if (osQuery != "" or versionQuery !="" or platform_versionQuery != "") else ""
 	devicesQuery = devicesQuery[:-4]
 
 
@@ -53,14 +59,41 @@ def search():
 	else:
 		licenseQuery = "" if license == "" else " type = '" + license + "' and"
 		statusQuery = "" if status == "" else " status = '" + status + "' and"
-		subscriptionsQuery = "SELECT user_id,id,type,status from subscriptions WHERE" + licenseQuery + statusQuery
+		subscriptionsQuery = "SELECT user_id from subscriptions WHERE" + licenseQuery + statusQuery
 		subscriptionsQuery = subscriptionsQuery[:-4]
 	# license = "" if request.args.get("license") == "" else "type = '" + request.args.get("license") + "'"
 	# query = "SELECT user_id,id,type,status from subscriptions WHERE " + license
 
+	query = "SELECT email,user_id,subscription_id,name,created_at from accounts WHERE "
+	if devicesQuery != "":
+		query = query + "user_id IN(" + devicesQuery + ") and "
+
+	if subscriptionsQuery != "":
+		query = query + "user_id IN(" + subscriptionsQuery + ") and "
+
+	if domain != "":
+		query = query + "email like '%" + domain + "' and "
 
 
-	query = devicesQuery
+	startQuery = ""
+	endQuery = ""
+	if start != "":
+		start_tuple = dt.datetime.strptime(start, "%B %d, %Y")
+		start_epoch = start_tuple.timestamp()
+		startQuery = str(start_epoch) + "<"
+		query = query + startQuery + "created_at and "
+
+	if end != "":
+		end_tuple = dt.datetime.strptime(end, "%B %d, %Y")
+		end_epoch = end_tuple.timestamp()
+		endQuery = "<" + str(end_epoch)
+		query = query + "created_at" + endQuery + " and "
+
+
+
+
+
+	query = query[:-4]
 
 	print(query)
 
