af9392ee7eb50077de36e22e45ba34faf4ca7035
Wystan Concepcion <concepcionwystan@gmail.com>
2018-07-16 15:24:37 +0800

    final

--- run.py
+++ run.py
@@ -42,9 +42,26 @@ def search():
 	multiple = request.form.get("multiple")
 	start = request.form.get("start")
 	end = request.form.get("end")
+	unsubs = request.form.get("unsubs")
+
+	first_name = request.form.get("first_name")
+	last_name = request.form.get("last_name")
+	license_type = request.form.get("license_type")
+
+
 
 	filters=[]
-	filters.extend([domain,version,status,platform_version,license,os,multiple,start,end])
+	filters.extend([domain,version,status,platform_version,license,os,multiple,start,end,unsubs])
+	realFilters = []
+	for item in filters:
+		if item == "on":
+			string = "multiple accounts" if multiple == "on" else "removed unsubs"
+			realFilters.append(string)
+		elif item != "" and item != [] and item != None:
+			realFilters.append(item)
+		else:
+			continue
+
 
 	
 
@@ -52,38 +69,29 @@ def search():
 
 	osQuery = ""
 	for item in os:
-		osQuery = osQuery + "platform = '" + item + "' or "
+		osQuery = osQuery + "devices.platform = '" + item + "' or "
 	osQuery = osQuery[:-4]
-	osQuery = "" if osQuery == "" else "(" + osQuery + ") and"
-	versionQuery = "" if version == "" else " app_version = '" + version + "' and"
-	platform_versionQuery = "" if platform_version == "" else " platform_version = '" + platform_version + "' and"
-	devicesQuery = "SELECT user_id from devices WHERE " + osQuery + versionQuery + platform_versionQuery if (osQuery != "" or versionQuery !="" or platform_versionQuery != "") else ""
-	devicesQuery = devicesQuery[:-4]
+	osQuery = "" if osQuery == "" else "(" + osQuery + ") and "
+	versionQuery = "" if version == "" else "devices.app_version = '" + version + "' and "
+	platform_versionQuery = "" if platform_version == "" else "devices.platform_version = '" + platform_version + "' and "
+	devicesQuery = osQuery + versionQuery + platform_versionQuery
 
 
-
-
-	if license == "" and status == "":
-		subscriptionsQuery = ""
-	else:
-		licenseQuery = "" if license == "" else " type = '" + license + "' and"
-		statusQuery = "" if status == "" else " status = '" + status + "' and"
-		subscriptionsQuery = "SELECT user_id from subscriptions WHERE" + licenseQuery + statusQuery
-		subscriptionsQuery = subscriptionsQuery[:-4]
+	licenseQuery = "" if license == "" else "subscriptions.type = '" + license + "' and "
+	statusQuery = "" if status == "" else "subscriptions.status = '" + status + "' and "
+	subscriptionsQuery = licenseQuery + statusQuery
 	
-	if multiple == "on":
-		subscriptionsQuery = subscriptionsQuery + " group by user_id having count(user_id) > 1"
 
-	query = "SELECT email,user_id,subscription_id,name,created_at from accounts WHERE "
-	if devicesQuery != "":
-		query = query + "user_id IN(" + devicesQuery + ") and "
+	query = "SELECT accounts.user_id,accounts.name,accounts.email,GROUP_CONCAT(DISTINCT(subscriptions.type)),accounts.created_at FROM accounts INNER JOIN devices ON accounts.user_id = devices.user_id INNER JOIN subscriptions ON accounts.user_id = subscriptions.user_id WHERE "
+	query = query + devicesQuery +  subscriptionsQuery
 
-	if subscriptionsQuery != "":
-		query = query + "user_id IN(" + subscriptionsQuery + ") and "
+	
 
 	if domain != "":
-		query = query + "email like '%" + domain + "' and "
+		query = query + "accounts.email like '%" + domain + "' and "
 
+	if unsubs == "on":
+		query = query + "accounts.email NOT IN(SELECT email from email_unsubscribers) and "
 
 	startQuery = ""
 	endQuery = ""
@@ -91,48 +99,56 @@ def search():
 		start_tuple = dt.datetime.strptime(start, "%B %d, %Y")
 		start_epoch = start_tuple.timestamp()
 		startQuery = str(start_epoch) + "<"
-		query = query + startQuery + "created_at and "
+		query = query + startQuery + "accounts.created_at and "
 
 	if end != "":
 		end_tuple = dt.datetime.strptime(end, "%B %d, %Y")
 		end_epoch = end_tuple.timestamp()
 		endQuery = "<" + str(end_epoch)
-		query = query + "created_at" + endQuery + " and "
+		query = query + "accounts.created_at" + endQuery + " and "
 
 
-	if query == "SELECT email,user_id,subscription_id,name,created_at from accounts WHERE ":
-		query = "SELECT email,user_id,subscription_id,name,created_at from accounts"
-		conn = mysql.connect()
-		cursor = conn.cursor()
-		cursor.execute(query)
-		data = cursor.fetchmany(5000)
-		with open('results.csv', 'w') as csvfile:
-			fieldnames = ['email', 'user_id', 'subscription_id','name','created_at']
-			writer = csv.writer(csvfile)
-			writer.writerow(fieldnames)
-			for item in data:
-				writer.writerow(item)
-	else:
-		query = query[:-4]
-		print(query)
-		conn = mysql.connect()
-		cursor = conn.cursor()
-		cursor.execute(query)
-		data = cursor.fetchall()
-		with open('results.csv', 'w') as csvfile:
-			fieldnames = ['email', 'user_id', 'subscription_id','name','created_at']
-			writer = csv.writer(csvfile)
-			writer.writerow(fieldnames)
-			for item in data:
-				# print(item[1])
-				query = "SELECT platform from devices where user_id=" + item[1]
-				# print(query)
-				cursor.execute(query)
-				os=cursor.fetchall()
-				# print(os)
-				writer.writerow(item)
+	query = query[:-4]
 
-	return render_template("filter.html",data=data,domain=domain,filters=filters)
+	if query == "SELECT accounts.user_id,accounts.name,accounts.email,GROUP_CONCAT(DISTINCT(subscriptions.type)),accounts.created_at FROM accounts INNER JOIN devices ON accounts.user_id = devices.user_id INNER JOIN subscriptions ON accounts.user_id = subscriptions.user_id WH":
+		query = "SELECT accounts.user_id,accounts.name,accounts.email,GROUP_CONCAT(DISTINCT(subscriptions.type)),accounts.created_at FROM accounts INNER JOIN devices ON accounts.user_id = devices.user_id INNER JOIN subscriptions ON accounts.user_id = subscriptions.user_id"
+	
+	query = query + " GROUP BY accounts.user_id" if multiple != "on" else query + " GROUP BY accounts.user_id having count(DISTINCT(devices.id)) > 1"
+	
+	print("+++++++++++++++++++")
+	print(query)
+	print("+++++++++++++++++++")
+	conn = mysql.connect()
+	cursor = conn.cursor()
+	cursor.execute(query)
+	data = cursor.fetchall()
+	with open('results.csv', 'w') as csvfile:
+		fieldnames = ['email']
+		if first_name == "on":
+			fieldnames.append('first_name')
+		if last_name == "on":
+			fieldnames.append('last_name')
+		if license_type == "on":
+			fieldnames.append('license_type')
+		writer = csv.writer(csvfile)
+		writer.writerow(fieldnames)
+		for item in data:
+			# print(item[1])
+			# query = "SELECT platform from devices where user_id=" + item[1]
+			# print(query)
+			# cursor.execute(query)
+			# os=cursor.fetchall()
+			# print(os)
+			row = [item[2]]
+			if first_name == "on":
+				row.append(item[1].split()[0])
+			if last_name == "on":
+				row.append(item[1].split()[-1])
+			if license_type == "on":
+				row.append(item[3])
+			writer.writerow(row)
+
+	return render_template("filter.html",data=data,domain=domain,filters=realFilters)
 	# try:
 	# 	return send_file('results.csv', attachment_filename='results.csv')
 	# except Exception as e:
